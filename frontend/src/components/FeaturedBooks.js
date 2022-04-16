21d937f9b3d2444026b1fa520ea2822e93f72e28
Kyei Samuel <kyeisamuel931@gmail.com>
2022-04-16 17:42:16 +0000

    landing page added

---
+++ frontend/src/components/FeaturedBooks.js
@@ -0,0 +1,96 @@
+import { Container, Fab, Stack, styled, Typography } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import BookCard from "./BookCard";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+export const CustomFab = styled(Fab)(({ theme }) => ({
+  color: "white",
+  backgroundColor: "#2f2e41",
+  "&:hover": {
+    backgroundColor: "#2f2e4180",
+  },
+}));
+function FeaturedBooks() {
+  const [Books, setBooks] = useState([]);
+  const recent = useRef();
+  useEffect(() => {
+    async function getBooks() {
+      const { data } = await axios.get("http://localhost:8000/api/books");
+      setBooks(data.results);
+    }
+    getBooks();
+  }, []);
+
+  const scrollLeft = () => {
+    recent.current.scrollLeft -= 250;
+  };
+
+  const scrollRight = () => {
+    recent.current.scrollLeft += 250;
+  };
+
+  return (
+    <Container
+      maxWidth={"lg"}
+      sx={{
+        height: 360,
+        position: "relative",
+        mt: 8,
+      }}
+    >
+      <Typography
+        variant="h4"
+        color={"#2f2e41"}
+        mb={2}
+        textAlign="center"
+        fontFamily={"Josefin Sans"}
+        fontWeight={700}
+      >
+        Recently Added Books
+      </Typography>
+      <CustomFab
+        size="medium"
+        onClick={scrollLeft}
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: 3,
+          zIndex: 2,
+        }}
+      >
+        <ArrowBackIosIcon fontSize="medium" />
+      </CustomFab>
+      <Stack
+        ref={recent}
+        direction="row"
+        overflow="hidden"
+        spacing={2}
+        sx={{
+          "& > img": {
+            borderRadius: 5,
+          },
+        }}
+      >
+        {Books.map((book) => (
+          <BookCard book={book} key={book.id} />
+        ))}
+      </Stack>
+      <CustomFab
+        size="medium"
+        onClick={scrollRight}
+        sx={{
+          position: "absolute",
+          top: "50%",
+          right: 3,
+          zIndex: 2,
+        }}
+      >
+        <ArrowForwardIosIcon fontSize="medium" />
+      </CustomFab>
+    </Container>
+  );
+}
+
+export default FeaturedBooks;
---
+++ frontend/src/components/HeroSection.js
@@ -0,0 +1,83 @@
+import React from "react";
+import Container from "@mui/material/Container";
+import { Box, Button, styled, Typography, useMediaQuery } from "@mui/material";
+import SearchField from "../components/searchField";
+import TopNav from "../components/TopNav";
+import heroImg from "../images/hero1.svg";
+
+export const CTAButton = styled(Button)(({ theme }) => ({
+  color: "white",
+  backgroundColor: "#2f2e41",
+  "&:hover": {
+    backgroundColor: "#6f6efc",
+    color: "white",
+  },
+}));
+function HeroSection() {
+  const large = useMediaQuery("(min-width:950px)");
+
+  return (
+    <Container
+      maxWidth="xl"
+      sx={{
+        // height: large ? "100vh" : "70vh",
+        width: "100vw",
+
+        display: "flex",
+        flexDirection: "column",
+        // gap: 2,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <TopNav />
+      <Box display={"flex"} alignItems="center">
+        <Box
+          p={2}
+          sx={{
+            width: large ? "50%" : "100%",
+            display: large ? "initial" : "flex",
+            flexDirection: large ? "initial" : "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Typography
+            variant="h3"
+            color={"#6f6efc"}
+            sx={{
+              fontFamily: "Josefin Sans",
+              fontWeight: 700,
+              textAlign: large ? "initial" : "center",
+            }}
+          >
+            Start reading your favorite books
+          </Typography>
+          <Typography
+            variant="body1"
+            color={"#2f2e41"}
+            textAlign={large ? "initial" : "center"}
+          >
+            Access over 5000 books for your research works, and personal
+            studies.
+            <br />
+            Filter your search by Author, title, subjects, etc.
+          </Typography>
+          <SearchField />
+          <CTAButton sx={{ mt: 2 }}>View Books</CTAButton>
+        </Box>
+        {large && (
+          <img
+            src={heroImg}
+            alt=""
+            width={"50%"}
+            height={"100%"}
+            style={{ objectFit: "contain" }}
+          ></img>
+        )}
+      </Box>
+    </Container>
+  );
+}
+
+export default HeroSection;
