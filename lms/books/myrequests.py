4ab2cc3fabfe202cddc4adb8f1a5887e0a4fb74f
Kyei Samuel <kyeisamuel931@gmail.com>
2022-05-27 11:17:58 +0000

    key accessed form env

--- lms/books/myrequests.py
+++ lms/books/myrequests.py
@@ -1,10 +1,11 @@
 from datetime import datetime
+import os
 import requests
 from dotenv import load_dotenv
 
-load_dotenv
+load_dotenv()
 
-key = "AIzaSyCsS71-7knB_kSOOMuIppGS5cw5ISUKsvo"
+key = os.getenv("GOOGLE_BOOKS_KEY")
 url = "https://www.googleapis.com/books/v1/volumes"
 
 def get_book_data(isbn: str):
